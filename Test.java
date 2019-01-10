cf1a2fce38b2a9903792c7a2c5131aeb3bbee3ff
LLJM <1308507643@qq.com>
2019-01-10 15:29:53 +0800

    next

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Define a variable a is：");
+        int a=5;//声明变量
+        System.out.println(a);
+    }
+}
