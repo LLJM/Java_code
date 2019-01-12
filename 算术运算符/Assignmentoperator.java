c1eb356d81c7ab13495de885d48a4d231137e531
liu <1308507643@qq.com>
2019-01-12 17:38:16 +0800

    operator

---
+++ 算术运算符/Assignmentoperator.java
@@ -0,0 +1,20 @@
+public class Assignmentoperator{
+    public static void main(String args[]){
+        int a=5;
+        int b=3;
+        b+=a;
+        System.out.println("b+=a="+b);
+        b=3;
+        b-=a;
+        System.out.println("b-=a="+b);
+        b=3;
+        b*=a;
+        System.out.println("b*=a="+b);
+        b=3;
+        b/=a;
+        System.out.println("b/=a="+b);
+        b=3;
+        b%=a;
+        System.out.println("b%=a="+b);
+    }
+}
